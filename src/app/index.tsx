e191335055e2b56cd6d72f2ed0c7dd3d79707672
Thaila Lemes <thailalemes@Thailas-MacBook-Air.local>
2024-04-02 19:44:00 -0300

    Initial commit

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native"
+
+export default function Home(){
+    return (
+        <View style={{ backgroundColor: "red", flex: 1 }}>
+            <Text>Hello React Native</Text>
+        </View>
+    )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+     content: ["./src/**/*.{ts,tsx}"],
+     prests: [require("nativewind/preset")],
+     theme: {
+        extend: {},
+      },
+      plugins: [],
+    }
